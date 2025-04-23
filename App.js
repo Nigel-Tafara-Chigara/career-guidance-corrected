b7da2ef0a10abf0e7150ce8afccff3e045b40b23
Sir Nijo <tafarachigara@gmail.com>
2025-04-23 09:07:53 +0200

    Web
    Web

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import Hello from "./components/register";
+import Login from "./components/log";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Mainpage from "./components/mainpg";
+import LandingPage from "./components/LandingPage";
+import ProgramDisplay from "./components/ProgramDisplay";
+import Login1 from "./components/logi";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />}></Route>
 
-
-//React navigation stack
-import RootStack from './navigators/RootStack';
-
-export default function App() {
-  return ( <RootStack/>
+        <Route path="/logi" element={<Login1 />}></Route>
+        <Route path="/log" element={<Login />}></Route>
+        <Route path="/register" element={<Hello />}></Route>
+        <Route path="/ProgramDisplay" element={<ProgramDisplay />}></Route>
+        <Route path="/mainpg" element={<Mainpage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-
+export default App;
